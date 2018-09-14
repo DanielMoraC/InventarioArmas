93fb4119688075a29247fa51e0b71511702799cf
DanielMoraC <43269179+DanielMoraC@users.noreply.github.com>
2018-09-14 14:53:03 +0200

    1

---
+++ mapa/Assets/SCRIPTS/Movimiento.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour {
+
+
+    //Para la cámara
+    public float horizontalSpeed;
+    public float verticalSpeed;
+    float h;
+    float v;
+
+    //Para el movimiento 
+    public float velocidad = 0.1f;
+
+    Vector3 Limite;
+    //private Rigidbody rbody;
+
+    public Rigidbody cuerpo;
+
+    //public GameObject cuerpo;
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //if (MenuPausa.GameIsPaused == false)
+        //{
+ 
+            h = horizontalSpeed * Input.GetAxis("Mouse X");
+            v = verticalSpeed * Input.GetAxis("Mouse Y");
+
+            transform.Rotate(0, h, 0);
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(0, 0, velocidad);
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(0, 0, -velocidad);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(-velocidad, 0, 0);
+            }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+           transform.Translate(velocidad, 0, 0);
+        }
+
+        
+
+
+
+        //}
+
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            cuerpo.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+            //transform.Translate(0, 2, 0);
+        }
+    }
+}
---
+++ mapa/Assets/SCRIPTS/loockcamera.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loockcamera : MonoBehaviour {
+
+    [SerializeField] private string mouseXInputName, mouseYInputName; //poner el nombre del imput del ratón
+    [SerializeField] private float mouseSensitivity; //definir la sensibilidad
+
+    [SerializeField] private Transform playerBody;
+
+    private float xAxisClamp; //para que no pase la camara por debajo o arriba y parezca que este dando una voltereta el personaje 
+
+    private void Awake()
+    {
+        LockCursor(); //bloquear cursor en el centro de la pantalla
+        xAxisClamp = 0.0f;
+    }
+
+    //bloquear cursor en el centro de la pantalla
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+
+        xAxisClamp += mouseY; //solo funciona en el axisY
+
+        if (xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(270.0f);
+        }
+        else if (xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(90.0f);
+        }
+
+        transform.Rotate(Vector3.left * mouseY);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        eulerRotation.y = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
