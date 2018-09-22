ecead87e4b8bbbc656723bb673cbee1f0c94861c
DanielMoraC <43269179+DanielMoraC@users.noreply.github.com>
2018-09-22 12:19:17 +0200

    Solo falta lo de saber el objeto equipado

---
+++ mapa/Assets/SCRIPTS/HealorDamage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealorDamage : MonoBehaviour {
+
+    public bool isDamaging;
+    public float damage = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.SendMessage((isDamaging) ? "TakeDamage" : "HealDamage", Time.deltaTime * damage);
+        }
+    }
+}
---
+++ mapa/Assets/SCRIPTS/Health.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+    public Image HPBar;
+    public Text ratioText;
+
+    public float hitpoint = 150;
+    public float maxhitpoint = 150;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateHealthbar();
+
+    }
+
+    private void UpdateHealthbar()
+    {
+        float ratio = hitpoint / maxhitpoint;
+        HPBar.rectTransform.localScale = new Vector3(ratio, 1, 1);
+        ratioText.text = (ratio * 100).ToString("0") + "%";
+    }
+
+    private void TakeDamage(float damage)
+    {
+        hitpoint -= damage;
+        if (hitpoint<0)
+        {
+            hitpoint = 0;
+            Debug.Log("Death");
+        }
+        UpdateHealthbar();
+    }
+
+    private void HealDamage(float heal)
+    {
+        hitpoint += heal;
+        if (hitpoint > maxhitpoint)
+        {
+            hitpoint = maxhitpoint;
+        }
+        UpdateHealthbar();
+    }
+}
