a16bcc998ddda416c438be43819ce7c4a33815a8
DanielMoraC <43269179+DanielMoraC@users.noreply.github.com>
2018-09-29 22:51:55 +0200

    ya dispara a lo torreta

---
+++ mapa/Assets/SCRIPTS/Ebala.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ebala : MonoBehaviour {
+
+    Rigidbody rb;
+    public float BulletSpeed;
+    GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddRelativeForce(0, 0, BulletSpeed, ForceMode.Impulse);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject,0);
+        }
+    }
+}
---
+++ mapa/Assets/SCRIPTS/Edisparar.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edisparar : MonoBehaviour {
+
+    public int disparando = 1;
+    Transform player;
+    public GameObject weapon;
+    public Transform gunEnd;
+    public int seconds;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(player);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && disparando ==1)
+        {
+            StartCoroutine("Shooting");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StopCoroutine("Shooting");
+            disparando = 1;
+        }
+    }
+    public IEnumerator Shooting()
+    {
+
+            disparando = 0;            
+            GameObject clone = Instantiate(weapon, gunEnd.position, gunEnd.rotation);
+            clone.SetActive(true);
+            yield return new WaitForSeconds(seconds);
+            disparando = 1;
+
+
+    }
+}
